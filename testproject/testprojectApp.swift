fc08a1c649bbbc3615e1c24e8105ee144c9c4b98
aximoris <aximoris@gmail.com>
2024-07-23 19:47:26 +0200

    Initial Commit

---
+++ testproject/testprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testprojectApp.swift
+//  testproject
+//
+//  Created by Maksim Borisov on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct testprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
